bdef0a2c2405c07d07fc8dd6f8c32084d8a4bb57
William J Ll Griffiths <604469+wgriffiths@users.noreply.github.com>
2024-09-27 13:01:23 +0100

    Add typescript tools

---
+++ lua/custom/plugins/typescript-tools.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opts = {},
+    config = function()
+      require("typescript-tools").setup {
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+        settings = {
+          tsserver_max_memory = 8092,
+          separate_diagnostic_server = false,
+        }
+      }
+    end,
+  },
+}
